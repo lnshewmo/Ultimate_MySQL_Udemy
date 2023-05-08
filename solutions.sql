a1aaba7ece76d2bbd161107f8b75cce24ead2290
Laura Miller <laura.shewmon@gmail.com>
2023-05-08 00:27:00 -0400

    completed solutions

--- solutions.sql
+++ solutions.sql
@@ -55,3 +55,15 @@ JOIN tags
 	ON photo_tags.tag_id = tags.id
 GROUP BY tags.id
 ORDER BY total DESC LIMIT 8;
+
+-- find users who have liked every single photo
+SELECT 
+	username, 
+    user_id, 
+    COUNT(*) AS num_likes
+FROM users
+JOIN likes
+	ON users.id=likes.user_id
+GROUP BY likes.user_id
+HAVING num_likes = (SELECT COUNT(*) FROM photos)
+ORDER BY username;
